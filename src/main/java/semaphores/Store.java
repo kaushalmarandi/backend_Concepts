8f8fc650f391c89fe60bed402aecd92487bb722a
Kaushal <kaushalmarandi@icloud.com>
2024-12-29 22:05:26 +0530

    semaphores

---
+++ src/main/java/semaphores/Store.java
@@ -0,0 +1,32 @@
+package semaphores;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Store {
+
+    private  int maxSize;
+    private List<Object> items;
+
+    public Store(int maxSize) {
+        this.maxSize = maxSize;
+        this.items = new ArrayList<>();
+    }
+
+
+    public int getMaxSize() {
+        return maxSize;
+    }
+
+    public void setMaxSize(int maxSize) {
+        this.maxSize = maxSize;
+    }
+
+    public List<Object> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Object> items) {
+        this.items = items;
+    }
+}
